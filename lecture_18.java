f206523c7a71444d2970449f3540a1337c6d3712
Aman772005 <amansinghtomar772005@gmail.com>
2026-08-21 11:41:13 +0530

    Autoboxing and Unboxing

---
+++ lecture_18.java
@@ -0,0 +1,19 @@
+public class lecture_18 {
+    public static void main(String[] args) {
+        // Autoboxing 
+
+        int x = 10 ;
+        Integer y = x ;
+
+        System.out.println(x);
+        System.out.println(y);
+
+        // Unboxing
+        
+        Integer a = 20 ;
+        int b = a ;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
